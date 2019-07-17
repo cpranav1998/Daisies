f5ad5668bdd2fb65cc6928d13b7cd7bec971ffea
Rahul12344 <therollingresu@gmail.com>
2019-07-17 14:17:28 -0700

    tentative implementation of filter feature (need to add more generality)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function greaterThanFilter(item,filterCategory,filterValue){
   listObj.filter((item) => {
   if (item.values().${filterCategory} > filterValue) {
@@ -17,6 +18,68 @@ function lessThanFilter(item,filterCategory,filterValue){
   }
   });
 }
+*/
 
+var options = {
+	valueNames: [
+		'categoryOne',
+		'categoryTwo',
+		'categoryThree',
+		{ data: ['categoryFour']}
+	],
+	page: 50,
+	pagination: true
+};
+var dataList = new List('data', options);
 
-listObj.filter();
+function resetList(){
+	dataList.search();
+	dataList.filter();
+	dataList.update();
+	$(".filter-all").prop('checked', true);
+	$('.filter').prop('checked', false);
+	$('.search').val('');
+	//console.log('Reset Successfully!');
+};
+
+function updateList(){
+  var values_one = $("input[name=categoryFour]:checked").val();
+	var values_two = $("input[name=categoryThree]:checked").val();
+	console.log(values_one, values_two);
+
+	dataList.filter(function (item) {
+		var filterOne = false;
+		var filterTwo = false;
+
+		if(values_one == "all")
+		{
+			filterOne = true;
+		} else {
+			filterOne = item.values().categoryFour == values_one;
+
+		}
+		if(values_two == null)
+		{
+			filterTwo = true;
+		} else {
+			filterTwo = item.values().address.indexOf(values_two) >= 0;
+		}
+		return filterTwo && filterOne
+	});
+	dataList.update();
+	//console.log('Filtered: ' + values_gender);
+}
+
+$(function(){
+  //updateList();
+  $("input[name=categoryFour]").change(updateList);
+	$('input[name=categoryThree]').change(updateList);
+
+	dataList.on('updated', function (list) {
+		if (list.matchingItems.length > 0) {
+			$('.no-result').hide()
+		} else {
+			$('.no-result').show()
+		}
+	});
+});
