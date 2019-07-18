ff8b326ac608a2b58b6aed702bc9963781eca1a6
pranav <pranav@factual.com>
2019-07-18 14:09:16 -0700

    Reconciled code

--- slytherinterns/lists/lists.py
+++ slytherinterns/lists/lists.py
@@ -26,33 +26,19 @@ def js_list(list_name: str, data: pd.DataFrame, options: ListOptions):
     Python wrapper to generate a complete listjs list for displaying a pandas df.
     """
     css = CSS(options.style)
-    html_div = div(cls="container", id=list_name)
-    print(css.add_fonts())
-    with html_div:
+
+    container = div(cls="container", id=list_name)
+
+    with container:
         css.add_fonts()
         style(css.add_css())
-        cont = div(cls='container')
-        with cont:
-            i(cls="fas fa-search")
-            input(cls="search form-control", placeholder="Search")
-
-
-
-def js_list(list_name: str, data: pd.DataFrame):
-    """
-    Python wrapper to generate a complete listjs list for displaying a pandas df.
-    """
-    container = div(cls="container", id=list_name)
-    with container:
         br()
         search_div()
         br()
-        div(input(cls="form-control", placeholder="Filter", onkeyup="filterFunction()"), cls="container")
+        filter_div()
         br()
-        sort_table = table(cls="table")
-        with sort_table:
-            make_table_header(data)
-            tbody(cls="list")
+        make_sort_table(data)
+
     td_setup = "".join(td(cls=field).render() for field in data.columns)
 
 
@@ -62,7 +48,7 @@ def js_list(list_name: str, data: pd.DataFrame):
 
     text_parser = make_text_parser()
     text_filter = make_filter()
-    js_code = f"""var options ={td_setup};
+    js_code = f"""var options ={options};
                   var values ={records};
                   var userList = new List('{list_name}', options, values);
                   {text_parser}
@@ -71,6 +57,20 @@ def js_list(list_name: str, data: pd.DataFrame):
     return {"html": container.render(), "js": js_code}
 
 
+def make_sort_table(data):
+    sort_table = table(cls="table")
+    with sort_table:
+        make_table_header(data)
+        tbody(cls="list")
+    return sort_table
+
+
+def filter_div():
+    cont = div(cls='container')
+    with cont:
+        i(cls="fas fa-search")
+        input(cls="form-control", placeholder="Filter", onkeyup="filterFunction()")
+    return cont
 
 def make_table_header(data):
     return thead().add(th(field, cls="sort", data_sort=field) for field in data.columns)
@@ -86,7 +86,12 @@ def make_options(fields, table_setup):
     return braces_wrap(vals + items)
 
 def search_div():
-    return div(input(cls="search form-control", placeholder="Search"), cls="container")
+    cont = div(cls="container")
+    with cont:
+        i(cls="fas fa-search")
+        input(cls="form-control", placeholder="Search")
+
+    return cont
 
 
 def make_text_parser():
